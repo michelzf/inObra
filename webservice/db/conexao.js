fc37067c479cdd78200a2e4e820d4effac387609
Michel Zarzour Filho <michelzarzour@outlook.com>
2017-01-28 12:57:12 -0200

    igiyviyigv

--- webservice/db/conexao.js
+++ webservice/db/conexao.js
@@ -5,7 +5,7 @@ dbCon = new Array();
 module.exports = function () {
     console.log(process.env.OPENSHIFT_MONGODB_DB_URL);
     console.log(process.env.OPENSHIFT_APP_NAME);
-    require('mongodb').MongoClient.connect("mongodb://userP5I:tDLxbOdNwVoDMiKq@localhost:63141/app", function (err, conn) {
+    require('mongodb').MongoClient.connect("mongodb://userP5I:tDLxbOdNwVoDMiKq@mongodb:63141/app", function (err, conn) {
         //mongodb://USUARIO:SENHA@URL:PORTA
         //    require('mongodb').MongoClient.connect("mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT", function (err, conn) {
         if (err) {
