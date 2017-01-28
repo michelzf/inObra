1518f44a303f504783d2a7a9830136b36d170b4a
Michel Zarzour Filho <michelzarzour@outlook.com>
2017-01-28 12:48:52 -0200

    jkbibiubiubiu

--- webservice/db/conexao.js
+++ webservice/db/conexao.js
@@ -5,7 +5,8 @@ dbCon = new Array();
 module.exports = function () {
     console.log(process.env.OPENSHIFT_MONGODB_DB_URL);
     console.log(process.env.OPENSHIFT_APP_NAME);
-    require('mongodb').MongoClient.connect("mongodb://admin:L1ekd6r9cy3K@5887c96289f5cf3c49000163-inopus2obra.rhcloud.com:63141/app", function (err, conn) {
+    require('mongodb').MongoClient.connect("mongodb://userP5I:tDLxbOdNwVoDMiKq@localhost:63141/app", function (err, conn) {
+        //mongodb://USUARIO:SENHA@URL:PORTA
         //    require('mongodb').MongoClient.connect("mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT", function (err, conn) {
         if (err) {
             console.log('Houve um erro na conexão com o BD. Erro: ' + err);
